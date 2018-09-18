a8706492d16392b80297206d69d28b6437551633
kyunghoon <aloecandy@gmail.com>
2018-09-18 18:53:23 +0900

    build test

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+
+void cudamain();
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "test");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(1);
+
+    while(ros::ok()){
+        cudamain();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
